dd97acc3b2b6c68ec463e52c5e431a094ae6adf6
Henry León <henriscoh1995@gmail.com>
2021-09-11 20:12:04 -0600

    Add, update and delte functionality added

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # API-RUST
 
-Api programmed in the Rust language with connection to a MySql database hosted on GCP.
+Api programmed in the Rust language with connection to a MySql database hosted on GCP.<br>
+To test each piece of code, you can comment and uncomment the blocks to interact with them and practice as you like.
 <br><br>
 
 ## Part 1.
 In this series of posts I will explore how to access MySQL from Rust. We will start simple and in later parts finish with developing a web service API.<br><br>
 The MySQL client API is quite easy to use. I love the way it converts data to whatever type you want to work with. Here we have used i32, String, and NaieveDate. This should cover the most common data types used in database programming.
+<br><br>
+
+## Part 2.
+In part 1 I discussed how to do basic query. In this article we will learn to do inserts and updates. We will continue to work with the PUBLICATION table.<br><br>
+In this part we learned to do insert, update and delete. Go to Part III where we will build a proper data access layer.
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ struct Publication {
     Date: String,
     Upvote: u64,
     Downvote: u64,
+    //last_changed_on: NaiveDate, in case you want a date
 }
 
 fn main() {
@@ -42,6 +43,7 @@ fn main() {
      as i32y String. The converted data is returned in a tuple where the elements are
      in the same order as the columns selected in the query. */
 
+
     //Query saving to vector - Collecting query result
     // let res: Vec<(i32, String, String)> = conn
     //     .query("select id, nombre, comentario from publicaciones")
@@ -52,6 +54,7 @@ fn main() {
     /* The query () function already converts the low-level bytes to our choice of data types
      so we don't have to do that. We had to explicitly mention the data type of the
      tuple. Otherwise the compiler has no way of knowing. */
+
 
     //Query using structs - Convert result to structured data
     /* Now we can map the query result to the Productobjects using query_map (). */
@@ -78,36 +81,119 @@ fn main() {
     /* The good thing is that we didn't have to specify the data type of the tuple. The compiler deduced it
      of the data types of the Product fields. */
 
+
     // Single element query.
     /* As a result, the query_first () function returns a Resultde Option. We need to unzip it
     twice to get the actual data for the row. */
-    let res = conn
-    .query_first("select id, nombre, comentario, fecha, upvotes, downvotes from publicaciones where id=8")
-    /* If you want to use named parameters, use the exec_first / () function instead of query_first */
-    // .exec_first(
-    //     "select id, nombre, comentario, fecha, upvotes, downvotes from publicaciones where ied=:id",
-    //     params!{
-    //         "id"=> 8
-    //     }, 
-    // )
-    //Unpack Result
-    .map(|row| {
-        //Unpack Option
-        row.map(|(Id, Name, Comment, Date, Upvote, Downvote)| Publication {
-            Id: Id,
-            Name: Name,
-            Comment: Comment,
-            Date: Date,
-            Upvote: Upvote,
-            Downvote: Downvote,
-        })
-    });
+    // let res = conn
+    // .query_first("select id, nombre, comentario, fecha, upvotes, downvotes from publicaciones where id=8")
+    // /* If you want to use named parameters, use the exec_first / () function instead of query_first */
+    // // .exec_first(
+    // //     "select id, nombre, comentario, fecha, upvotes, downvotes from publicaciones where ied=:id",
+    // //     params!{
+    // //         "id"=> 8
+    // //     }, 
+    // // )
+    // //Unpack Result
+    // .map(|row| {
+    //     //Unpack Option
+    //     row.map(|(Id, Name, Comment, Date, Upvote, Downvote)| Publication {
+    //         Id: Id,
+    //         Name: Name,
+    //         Comment: Comment,
+    //         Date: Date,
+    //         Upvote: Upvote,
+    //         Downvote: Downvote,
+    //     })
+    // });
+    // match res.unwrap() {
+    //     Some(publi) => println!(
+    //         "{}, {}, {}, {}, {}, {}", // {:?} is in case the data is type date
+    //         publi.Id, publi.Name, publi.Comment, publi.Date, publi.Upvote, publi.Downvote
+    //     ),
+    //     None => println!("Sorry no publication found."),
+    // }
 
-    match res.unwrap() {
-        Some(publi) => println!(
-            "{}, {}, {}, {}, {}, {}",
-            publi.Id, publi.Name, publi.Comment, publi.Date, publi.Upvote, publi.Downvote
-        ),
-        None => println!("Sorry no publication found."),
-    }
+
+    // Inserting data
+    // This is in case they need to use the date.
+    // fn today() -> NaiveDate {
+    //     let l = Local::today();
+    //     NaiveDate::from_ymd(l.year(), l.month(), l.day())
+    // }
+    // let stmt = conn.prep("insert into publicaciones (nombre, comentario, fecha, upvotes, downvotes) values (:nombre, :comentario, :fecha, :upvotes, :downvotes)")
+    //     .unwrap();
+    // conn.exec_drop(
+    //     &stmt,
+    //     params! {
+    //         "nombre" => "Rust 1",
+    //         "comentario" => "Comentario simple de Rust 1",
+    //         "fecha" => "11/09/2021",
+    //         //Pos i se desea usar la fecha "upvotes" => today(),
+    //         "upvotes" => 15,
+    //         "downvotes" => 42,
+    //     },
+    // )
+    // .unwrap();
+    // conn.exec_drop(
+    //     &stmt,
+    //     params! {
+    //         "nombre" => "Rust 1",
+    //         "comentario" => "Comentario simple de Rust 2",
+    //         "fecha" => "11/09/2021",
+    //         //Pos i se desea usar la fecha "upvotes" => today(),
+    //         "upvotes" => 5,
+    //         "downvotes" => 420,
+    //     },
+    // )
+    // .unwrap();
+    /* The paramsmacro makes it easy to supply named parameter values. Here drop exec_drop ()
+     means that no result data is returned. Which is fine for inserting, updating, and
+     remove the SQL type.
+     If you do a lot of inserts, it will be faster to compile the SQL as a prepared statement. */
+
+
+    // Get generated primary key
+    // let stmt = conn.prep("insert into publicaciones (nombre, comentario, fecha, upvotes, downvotes) values (:nombre, :comentario, :fecha, :upvotes, :downvotes)")
+    //     .unwrap();
+    // conn.exec_drop(
+    //     &stmt,
+    //     params! {
+    //         "nombre" => "Rust 1",
+    //         "comentario" => "Comentario simple de Rust 3",
+    //         "fecha" => "11/09/2021",
+    //         "upvotes" => 70,
+    //         "downvotes" => 2,
+    //     },
+    // ).unwrap();
+    // println!("Last generated key: {}", conn.last_insert_id());
+    /* In our case, the primary key is generated by MySQL. We can get it back by calling conn.last_insert_id (). */
+
+
+    // Update and Delete
+    /* They are similar to insert, example of update and delete respectively */
+    // let stmt = conn
+    //     .prep("update publicaciones set nombre=:nombre, comentario=:comentario where id=:id")
+    //     .unwrap();
+    // conn.exec_drop(
+    //     &stmt,
+    //     params! {
+    //         "id" => 32,
+    //         "nombre" => "Rust 1",
+    //         "comentario" => "Comentario simple de Rust 3 modificado",
+    //         //"last_update" => NaiveDate::from_ymd(2020, 10, 12), Por si se desea la fecha
+    //     },
+    // )
+    // .unwrap();
+
+    // let stmt = conn
+    //     .prep("delete from publicaciones where id=:id")
+    //     .unwrap();
+    // conn.exec_drop(
+    //     &stmt,
+    //     params! {
+    //         "id" => 21,
+    //     },
+    // )
+    // .unwrap();
 }
